ab8d930fb63770cbeea034da1d2c8867aad318c4
Joao Vitor Garcia <jvdasilvaeirasgarcia@gmail.com>
2022-12-17 13:10:09 -0300

    Refactor: Separating app layers and tests

---
+++ src/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,19 @@
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
+import { SendNotification } from './send-notification';
+
+describe('Send notification', () => {
+  it('should be able to send a notification', async () => {
+    const NotificationsRepository = new InMemoryNotificationsRepository();
+
+    const sendNotification = new SendNotification(NotificationsRepository);
+
+    const { notification } = await sendNotification.execute({
+      content: 'New notification',
+      category: 'social',
+      recipientId: 'example-recipient-id',
+    });
+
+    expect(NotificationsRepository.notifications).toHaveLength(1);
+    expect(NotificationsRepository.notifications[0]).toEqual(notification);
+  });
+});
